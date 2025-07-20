de61faa037035e38f7d73458884d28354967929a
Medy <mannomedy@gmail.com>
2025-07-20 22:42:35 +0300

    add changes

---
+++ src/components/Article.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Article({title, date = "January 1, 1970", preview, minutes}) {
+    const getEmojiInducater = (minutes) =>{
+        if (minutes < 30){
+            const cups = Math.ceil(minutes /5)
+            return "☕️".repeat(cups) 
+        }else{
+            const bentos = Math.ceil(minutes /10)
+            return "🍱".repeat(bentos);
+        }
+    }
+
+
+
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>
+                {date}
+            </small>
+            <p>{preview}</p>
+
+        </article>
+
+  )
+}
+
+export default Article
